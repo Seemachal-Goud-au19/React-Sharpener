65f833f1e60b7dfc70dcabb07f64412ab62f53db
Seemachal <seemachal.goud@infobeans.com>
2023-11-01 23:16:15 +0530

    props passed to expenseItem component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from "react";
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 2345, date: new Date(20203, 10, 29), location: 'Surat' },
+    { title: 'Health Insurance', amount: 2500, date: new Date(20203, 10, 29), location: 'Pune' },
+    { title: 'Party', amount: 2400, date: new Date(20203, 10, 29), location: 'Ahmedabad' }
+  ]
   return (
     <>
-      <ExpenseItems/>
+   { expenses.map((expense)=> <ExpenseItems expense={expense} />)
+     
+   }
     </>
   )
 };
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import './ExpenseItems.css'
 
-const ExpenseItems = () => {
+const ExpenseItems = ({expense}) => {
+    const formattedDate = expense.date.toLocaleDateString();
+
     return (
-        <div>
-            <ul>Expense Items</ul>
-            <li>Food Rs 10</li>
-            <li> Petrol Rs 100</li>
-            <li>Movies Rs 200</li>
+        <div className='expense-item'>
+            <div>{formattedDate}</div>
+            <div>{expense.location}</div>
+            <div className='expense-item_description'>
+                <h2>{expense.title}</h2>
+                <div className='expense-item_price'>{expense.amount}</div>
+            </div>
         </div>
     )
 }
