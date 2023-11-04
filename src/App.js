2147b35c8015641a06b39ef17fb2778edd0b26c1
Seemachal <seemachal.goud@infobeans.com>
2023-11-04 11:53:00 +0530

    delete button for expense Item added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItems from "./components/Expenses/ExpenseItems";
 
+const expensesList = [
+  { id: 1, title: 'Car Insurance', amount: 2345, date: new Date(20203, 10, 29), location: 'Surat' },
+  { id: 2, title: 'Health Insurance', amount: 2500, date: new Date(20203, 10, 29), location: 'Pune' },
+  { id: 3, title: 'Party', amount: 2400, date: new Date(20203, 10, 29), location: 'Ahmedabad' }
+]
+
 function App() {
-  const expenses = [
-    { title: 'Car Insurance', amount: 2345, date: new Date(20203, 10, 29), location: 'Surat' },
-    { title: 'Health Insurance', amount: 2500, date: new Date(20203, 10, 29), location: 'Pune' },
-    { title: 'Party', amount: 2400, date: new Date(20203, 10, 29), location: 'Ahmedabad' }
-  ]
+  const [expenses, setExpenses] = useState([...expensesList]);
+
+
+  const handleDeleteExpenseItems = (id) => {
+    setExpenses(expenses.filter((item) => item.id !== id))
+  }
   return (
     <>
-   { expenses.map((expense)=> <ExpenseItems expense={expense} />)
-     
-   }
+      {expenses.map((expense) => <ExpenseItems expense={expense} handleDeleteExpenseItems={handleDeleteExpenseItems} />)
+
+      }
     </>
   )
 };
--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -2,16 +2,17 @@ import React from 'react'
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css'
 
-const ExpenseItems = ({expense}) => {
-    
+const ExpenseItems = ({ expense, handleDeleteExpenseItems }) => {
+
     return (
         <div className='expense-item'>
-           <ExpenseDate date={expense.date}/>
+            <ExpenseDate date={expense.date} />
             <div>{expense.location}</div>
             <div className='expense-item_description'>
                 <h2>{expense.title}</h2>
                 <div className='expense-item_price'>{expense.amount}</div>
             </div>
+            <div onClick={() => handleDeleteExpenseItems(expense.id)} style={{ cursor: 'pointer'}}>Delete</div>
         </div>
     )
 }
