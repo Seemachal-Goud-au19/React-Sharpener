61efbe8dc0256db907d5082cb4306dd31e6471ab
Seemachal <seemachal.goud@infobeans.com>
2023-11-04 13:15:03 +0530

    usState used to change amount and title

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css'
 
 const ExpenseItems = ({ expense, handleDeleteExpenseItems }) => {
+    const [title, setTitle] = useState(expense.title);
+    const [amount, setAmount] = useState(expense.amount);
+    const handleChangeTitle = () => {
+        setTitle('Update')
+        
+    }
 
+    const handleChangeAmount =()=>{
+        setAmount(100+'$')
+    }
     return (
         <div className='expense-item'>
             <ExpenseDate date={expense.date} />
             <div>{expense.location}</div>
             <div className='expense-item_description'>
-                <h2>{expense.title}</h2>
-                <div className='expense-item_price'>{expense.amount}</div>
+                <h2>{title}</h2>
+                <div className='expense-item_price'>{amount}</div>
             </div>
-            <div onClick={() => handleDeleteExpenseItems(expense.id)} style={{ cursor: 'pointer'}}>Delete</div>
+            <div onClick={handleChangeAmount} style={{ cursor: 'pointer' }}>Change Amount</div>
+            <div onClick={handleChangeTitle} style={{ cursor: 'pointer' }}>Change title</div>
+            <div onClick={() => handleDeleteExpenseItems(expense.id)} style={{ cursor: 'pointer' }}>Delete</div>
         </div>
     )
 }
