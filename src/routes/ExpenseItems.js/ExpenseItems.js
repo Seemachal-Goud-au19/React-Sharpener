aa3681ef37c9da8909585c4a6fa16ca0767ea6c6
Seemachal <seemachal.goud@infobeans.com>
2023-11-06 23:11:44 +0530

    Key added to the ExpensItem list

--- src/routes/ExpenseItems.js/ExpenseItems.js
+++ src/routes/ExpenseItems.js/ExpenseItems.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import ExpenseItem from '../../components/Expenses/ExpenseItem';
 
 
-const ExpenseItems = ({expenseList, setExpenseList}) => {
-  
-  console.log('expenseList',expenseList)
+const ExpenseItems = ({ expenseList, setExpenseList }) => {
+
+  console.log('expenseList', expenseList)
   const handleDeleteExpenseItems = (id) => {
     setExpenseList(expenseList.filter((item) => item.id !== id))
   }
   return (
     <>
-      {expenseList.map((expense) => <ExpenseItem expense={expense} handleDeleteExpenseItems={handleDeleteExpenseItems} />)
+      {expenseList.map((expense) => <ExpenseItem key={expense.id} expense={expense}
+        handleDeleteExpenseItems={handleDeleteExpenseItems} />)
 
       }
     </>
